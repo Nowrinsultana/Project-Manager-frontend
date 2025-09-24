d1cde48fd63dfae1009d94fec2ecbf31f5371906
Nowrinsultana <c213250@ugrad.iiuc.ac.bd>
2025-09-24 19:50:27 +0600

    component call a & import

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import Contact from './Component/Contact'
+import Footer from './Component/Footer'
+import Heder from './Component/Heder'
+import Hero from './Component/Hero'
+
 const App = () => {
-  return <div></div>
+  return (
+    <div>
+      <Heder />
+      <Hero />
+      <Contact />
+      <Footer />
+    </div>
+  )
 }
 
 export default App
